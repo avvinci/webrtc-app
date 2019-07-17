da909514b39462049d661c56fff96cd2b8522b1d
avvinci <vinciabhinav7@gmail.com>
2019-07-17 19:08:37 +0530

    first task done

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,22 @@
-'use strict';
+"use strict";
 
+const mediaStreamConstraints = {
+  video: true
+};
+
+const localVideo = document.querySelector("video");
+let localStream;
+
+function getLocalMediaStream(mediaStream) {
+  localStream = mediaStream;
+  localVideo.srcObject = mediaStream;
+}
+
+function handleLocalMediaStreameError(error) {
+  console.log("navigator.getUserMedia erro:", error);
+}
+ 
+navigator.mediaDevices
+  .getUserMedia(mediaStreamConstraints)
+  .then(getLocalMediaStream)
+  .catch(handleLocalMediaStreameError);
